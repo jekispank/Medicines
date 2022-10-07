22dff70b7de07f2697952b2d459ba2394978fd23
Evgeny <jekispank@mail.ru>
2022-10-07 16:42:39 +0300

    Adding PillItemActivity.kt and layout for adding and editing pillItem.

---
+++ app/src/main/java/com/example/medicines/presentation/PillItemActivity.kt
@@ -0,0 +1,12 @@
+package com.example.medicines.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.medicines.R
+
+class PillItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pill_item)
+    }
+}
