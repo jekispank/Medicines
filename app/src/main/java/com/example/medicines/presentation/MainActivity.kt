4ee7fc3548e44ccc23b0683e2492d683d7b92ff7
Evgeny <jekispank@mail.ru>
2022-10-28 13:52:53 +0300

    Creating layout for landscape orientation.

--- app/src/main/java/com/example/medicines/presentation/MainActivity.kt
+++ app/src/main/java/com/example/medicines/presentation/MainActivity.kt
@@ -1,22 +1,26 @@
 package com.example.medicines.presentation
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentContainerView
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
+import com.example.medicines.R
 import com.example.medicines.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
     private lateinit var viewModel: MainViewModel
     private lateinit var pillListAdapter: PillListAdapter
+    private var pillItemContainer: FragmentContainerView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        pillItemContainer = binding.pillItemContainer
         setupRecyclerView()
 
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
@@ -24,9 +28,24 @@ class MainActivity : AppCompatActivity() {
             pillListAdapter.submitList(it)
         }
         binding.buttonAddPillItem.setOnClickListener{
-            val intent = PillItemActivity.newIntentAddItem(this)
-            startActivity(intent)
+            if (isOnePaneMode()) {
+                val intent = PillItemActivity.newIntentAddItem(this)
+                startActivity(intent)
+            }
+            else launchFragment(PillItemFragment.newInstanceAddItem())
         }
+    }
+
+    private fun isOnePaneMode(): Boolean {
+        return pillItemContainer == null
+    }
+
+    private fun launchFragment(fragment: Fragment) {
+        supportFragmentManager.popBackStack()
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.pill_item_container, fragment)
+            .addToBackStack(null)
+            .commit()
     }
 
     fun setupRecyclerView() {
@@ -74,9 +93,11 @@ class MainActivity : AppCompatActivity() {
 
     private fun setupClickListener() {
         pillListAdapter.onPillItemClickListener = {
-            Log.d("ClickListener", "Click-click on ${it.id}")
-            val intent = PillItemActivity.newIntentEditItem(this, it.id)
-            startActivity(intent)
+            if (isOnePaneMode()) {
+                val intent = PillItemActivity.newIntentEditItem(this, it.id)
+                startActivity(intent)
+            }
+            else launchFragment(PillItemFragment.newInstanceEditItem(it.id))
         }
     }
 
